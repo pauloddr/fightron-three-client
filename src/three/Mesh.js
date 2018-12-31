885bc3da0188f5989c290ff45ca29ecd29790c92
@pauloddr <git@pauloddr.com>
2018-12-31 18:39:13 +0000

    fix lint errors

--- src/three/Mesh.js
+++ src/three/Mesh.js
@@ -1,8 +1,8 @@
-import { Mesh as ThreeMesh, MeshBasicMaterial, BackSide } from 'three'
+import { Mesh as ThreeMesh } from 'three'
 // import { relativeScale } from '../utils/relative-vectors'
 
-const outlineMaterial = new MeshBasicMaterial({ color: 'black', side: BackSide })
-const outlineSize = 2
+// const outlineMaterial = new MeshBasicMaterial({ color: 'black', side: BackSide })
+// const outlineSize = 2
 
 export class Mesh extends ThreeMesh {
   constructor (geometry, material) {
